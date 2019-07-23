d72dc0b2ed508c034891130126243156c9a4fb0b
LeonardoM <LeonardoM@leapthought.co.nz>
2019-07-23 12:46:15 +1200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Security.Principal;
+using System.Text;
+
+namespace HostChange
+{
+    class Program
+    {
+        public const string CfgStart = "#CFG_START>";
+        public const string CfgEnd = "#END";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("HostChange v0.1\n");
+
+            if (!IsAdministrator())
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("HostChange requires ADMIN privileges to be able to change hosts.");
+                Console.ForegroundColor = ConsoleColor.White;
+                Environment.Exit(-1);
+            }
+
+            if (args.Length == 0)
+            {
+                Console.WriteLine(@"Usage:   C:\HostChange [<operation>] <sectionName>");
+                Console.WriteLine("   -list = list all available section names ");
+                Console.WriteLine("   -fulllist = list all available sections values ");
+                Environment.Exit(-2);
+            }
+
+            var opList = args[0].ToLower().Equals("-list");
+            var opFullList = args[0].ToLower().Equals("-fulllist");
+
+            var hostsContent = File.ReadAllLines(Path.Combine(Environment.SystemDirectory, @"Drivers\Etc\Hosts."));
+
+            if (opList || opFullList)
+            {
+                ListSections(hostsContent, opFullList);
+                Environment.Exit(0);
+            }
+
+            var sectionName = args[0];
+
+            var outputLines = new StringBuilder();
+            var uncomment = false;
+
+            for (int line = 0; line < hostsContent.Length; line++)
+            {
+                if (hostsContent[line].ToUpper().StartsWith(CfgStart))
+                {
+                    var (sName, _) = GetSectionName(hostsContent[line]);
+
+                    if (sName.ToLower().Equals(sectionName.ToLower()))
+                    {
+                        uncomment = true;
+                        outputLines.AppendLine(hostsContent[line]);
+                        continue;
+                    }
+                }
+
+                if (uncomment)
+                {
+                    if (hostsContent[line].ToUpper().StartsWith(CfgEnd))
+                    {
+                        uncomment = false;
+                        outputLines.AppendLine(hostsContent[line]);
+                        continue;
+                    }
+
+                    while (hostsContent[line].StartsWith("#"))
+                    {
+                        hostsContent[line] = hostsContent[line].Substring(1);
+                    }
+                    outputLines.AppendLine(hostsContent[line]);
+                }
+                else
+                {
+                    if (!hostsContent[line].StartsWith("#"))
+                    {
+                        outputLines.Append("#");
+                    }
+
+                    outputLines.AppendLine(hostsContent[line]);
+                }
+            }
+
+            File.WriteAllText(Path.Combine(Environment.SystemDirectory, @"Drivers\Etc\Hosts."), outputLines.ToString());
+
+            FlushDns();
+        }
+
+        private static void ListSections(string[] hostEntries, bool showFullList)
+        {
+            var found = false;
+
+            var previousConsoleColor = Console.ForegroundColor;
+
+            for (int line = 0; line < hostEntries.Length - 1; line++)
+            {
+                if (hostEntries[line].StartsWith(CfgStart))
+                {
+                    var (sName, sDesc) = GetSectionName(hostEntries[line]);
+
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.Write($"#{sName} -> ");
+                    Console.ForegroundColor = previousConsoleColor;
+                    Console.WriteLine(sDesc);
+
+                    found = true;
+                }
+                else
+                {
+                    if (showFullList)
+                    {
+                        if (!hostEntries[line].StartsWith("#"))
+                        {
+                            Console.ForegroundColor = ConsoleColor.Blue;
+                            Console.WriteLine(hostEntries[line]);
+                            Console.ForegroundColor = previousConsoleColor;
+                        }
+                        else
+                        {
+                            Console.WriteLine(hostEntries[line]);
+                        }
+                    }
+                }
+            }
+
+            if (!found)
+            {
+                Console.WriteLine("No sections found.");
+            }
+        }
+
+
+        private static (string, string) GetSectionName(string text)
+        {
+            string sectionName = "";
+            string descriptionName = "";
+
+            var content = text.Replace(CfgStart, "");
+
+            if (content.IndexOf("[", StringComparison.Ordinal) > 0)
+            {
+                if (content.IndexOf("]", StringComparison.Ordinal) > 0)
+                {
+                    sectionName = content.Substring(0, content.IndexOf("[", StringComparison.Ordinal));
+                    descriptionName = content.Substring(content.IndexOf("[", StringComparison.Ordinal) + 1);
+                    descriptionName = descriptionName.Remove(descriptionName.Length - 1);
+                }
+                else
+                {
+                    sectionName = content;
+                }
+            }
+
+            return (sectionName.Trim(), descriptionName.Trim());
+        }
+
+        public static bool IsAdministrator()
+        {
+            var identity = WindowsIdentity.GetCurrent();
+            var principal = new WindowsPrincipal(identity);
+            return principal.IsInRole(WindowsBuiltInRole.Administrator);
+        }
+
+        private static void FlushDns()
+        {
+            Process myProcess = new Process();
+
+            try
+            {
+                myProcess.StartInfo.UseShellExecute = false;
+                myProcess.StartInfo.FileName = Path.Combine(Environment.SystemDirectory, @"Ipconfig.exe");
+                myProcess.StartInfo.Arguments = "/flushdns";
+                myProcess.StartInfo.CreateNoWindow = true;
+                myProcess.Start();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,69 @@
-# hostchange
-Switch windows hosts file configuration easily
+﻿# HostChange
+HostChange is a console application to help manage different configurations for a windows hosts file.. 
+
+### How does it work?
+This app will download a list of hosts that are considered malicious, and tells your computer to avoid the connection to them by adding that list to a local file called `hosts`.
+It uses a consolidated hosts list provided by **Steven Black**. You can check his [Github repository](https://github.com/StevenBlack/hosts) for more information.
+
+### What do I need to run it?
+HostChange is console application that requires a DotNet version 4.7.2+. Given that a system file is modified, **administration privileges** are required too.
+
+### How do create sections?
+To create a section, add a new line starting with **#CFG_START>**, followed by the section name, and optionally with a description enclosed by []. On the next lines, add the IPs entries and finish the section with a new line containing **#END**
+
+Example
+#CFG_START>MySectionName [My section Description]
+xx.xx.xx.xx      url
+xx.xx.xx.xx      url
+xx.xx.xx.xx      url
+#END
+
+In this example a new section named **MySectionName** was created. This section has as description **My section Description** and 3 IPs.
+
+So you can create several sections and toggle them using HostChange later. Ex.
+
+```
+#CFG_START>Config1 [Sample Configuration 1]
+#103.20.70.200    demo.acme.co.xx
+#103.20.70.201    off.acme.co.xx
+#END
+#CFG_START>Config2 [Sample Configuration 2]
+#203.30.88.200    prod.acme.co.xx
+#203.30.88.201    on.acme.co.xx
+#END
+#CFG_START>Config3 [Sample Configuration 3]
+#211.97.12.200    test.acme.co.xx
+#211.97.12.201    onoff.acme.co.xx
+#END
+```
+
+### How do i run it?
+Open command prompt, as Administrator, and execute HostChange.exe.
+
+* HostChange.exe -list
+will list all sections configured in your hosts.
+```
+#Config1 -> Sample Configuration 1
+#Config2 -> Sample Configuration 2
+#Config3 -> Sample Configuration 3
+```
+
+* HostChange.exe -fulllist
+will list all hosts. contents
+```
+#CFG_START>Config1 [Sample Configuration 1]
+#103.20.70.200    demo.acme.co.xx
+#103.20.70.201    off.acme.co.xx
+#END
+#CFG_START>Config2 [Sample Configuration 2]
+#203.30.88.200    prod.acme.co.xx
+#203.30.88.201    on.acme.co.xx
+#END
+#CFG_START>Config3 [Sample Configuration 3]
+#211.97.12.200    test.acme.co.xx
+#211.97.12.201    onoff.acme.co.xx
+#END
+```
+
+* HostChange.exe Config1
+Will change the current configuration to **Config1**. You can switch configuration at any time. After HostChange apply the new configuration it will perform a flushdns.
